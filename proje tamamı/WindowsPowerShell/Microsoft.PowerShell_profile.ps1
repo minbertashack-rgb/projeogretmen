cc649f672a100c958cef5e3c42f6269c32ba6895
CemreEren <142149021+CemreEren@users.noreply.github.com>
2026-03-24 18:45:28 +0300

    fff

--- proje tamamı/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,4 +0,0 @@
-﻿# VSCode terminali açılınca ortak profili yükle
-if (Test-Path $PROFILE.CurrentUserAllHosts) { . $PROFILE.CurrentUserAllHosts }
-$rt = "$HOME\OneDrive\Belgeler\PowerShell\regtech_profile.ps1"
-if (Test-Path $rt) { . $rt }
--- projeogretmenler/docverse_backend/docverse/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DocverseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'docverse'
